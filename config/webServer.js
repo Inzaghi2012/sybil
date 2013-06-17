81c791c410ee5d5bd48db34403ec2f6d153d86f7
giyyapan <giyyapan@gmail.com>
2013-06-17 17:02:06 +0800

    change name rss->source ,start->offset . without test

---
+++ config/webServer.js
@@ -0,0 +1,16 @@
+(function() {
+  var feedServerConfig;
+
+  feedServerConfig = require("./feedServerConfig.coffee");
+
+  exports.localPort = 40134;
+
+  exports.port = 3001;
+
+  exports.frontWebSocketPort = 41121;
+
+  exports.feedServerPort = feedServerConfig.port;
+
+  exports.feedServerHost = feedServerConfig.host;
+
+}).call(this);
